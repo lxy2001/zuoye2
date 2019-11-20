7d5f65ba967828be72ea07e51c25d0e3d5154d77
20181106279 <1771478860@qq.com>
2019-11-20 10:49:28 +0800

    2
    2

--- zuoye2/ViewController.swift
+++ zuoye2/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number1: UITextField!
+
+    @IBOutlet weak var operaa: UITextField!
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    @IBOutlet weak var number3: UITextField!
+    
+    @IBAction func jia(_ sender: UIButton) {
+        operaa.text = "+"
+    }
+    
+    @IBAction func jian(_ sender: UIButton) {
+        operaa.text = "-"
+    }
+    
+    @IBAction func cheng(_ sender: UIButton) {
+        operaa.text = "*"
+    }
+    
+    @IBAction func chu(_ sender: UIButton) {
+        operaa.text = "/"
+    }
+    
+    @IBAction func jisuan(_ sender: UIButton) {
+        if operaa.text == "+"{
+            number3.text = String(Double(number1.text!)!+Double(number2.text!)!)
+        }
+        if operaa.text == "-"{
+            number3.text = String(Double(number1.text!)!-Double(number2.text!)!)
+        }
+        if operaa.text == "*"{
+            number3.text = String(Double(number1.text!)!*Double(number2.text!)!)
+        }
+        if operaa.text == "/"{
+            number3.text = String(Double(number1.text!)!/Double(number2.text!)!)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
